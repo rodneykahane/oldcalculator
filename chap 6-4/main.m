6cefa1a8e75d141fc1d1d55582e052ec3b006918
Rodney Kahane <rodney@rodneys-macbook-pro.local>
2017-10-16 19:28:39 -0400

    Initial Commit

---
+++ chap 6-4/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  chap 6-4
+//
+//  Created by Rodney Kahane on 10/16/17.
+//  Copyright © 2017 Rodney Kahane. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
